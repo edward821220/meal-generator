99518af023279cc21d4319b69b948cdd684d785a
Edward <edward821220@gmail.com>
2022-06-20 16:58:10 +0800

    update comments

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 const request = new XMLHttpRequest();
 
+// 先寫一個取得資料的 function
 function getData(cb) {
+// 連接 API
   request.open(
     "GET",
     "https://www.themealdb.com/api/json/v1/1/random.php",
     true
   );
+// 載入完成後執行，如果連接成功就將 JSON 資料轉成 object，失敗的話就印出錯誤訊息 
   request.onload = function (e) {
     if (this.status >= 200 && this.status < 400) {
       console.log("success");
       let datas = JSON.parse(this.response).meals[0];
+// 資料轉成 object 後丟到 callback function 處理
       cb(datas);
     } else {
       console.log("err");
       console.log(e);
     }
   };
-
+// requests 發生錯誤就印出來 
   request.onerror = function (e) {
     console.log("error");
     console.log(e);
@@ -25,6 +29,7 @@ function getData(cb) {
   request.send();
 }
 
+// 藉由 DOM 操作將資料加到 HTML 的函數
 function addHTML(results) {
   const meal = document.querySelector(".meal");
   meal.innerHTML = "";
@@ -62,9 +67,12 @@ function addHTML(results) {
   `;
 }
 
+// 對按鈕增加一個 click 事件
 const meal_btn = document.querySelector(".btn button");
 meal_btn.addEventListener("click", () => {
+//   click 後就執行 getData function
   getData((datas) => {
+//   把取得的資料 datas 整理成想要的 results
     let results = {};
     results.meal = datas.strMeal;
     results.meal_img = datas.strMealThumb;
@@ -81,6 +89,7 @@ meal_btn.addEventListener("click", () => {
         );
       }
     }
+//  把 results 丟到 addHTML function 去執行，將資料加到 HTML 內
     addHTML(results);
   });
 });
